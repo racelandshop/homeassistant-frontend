b1ac086d8315bc220c36b8ccbe2e0cbd15bfae0a
J. Nick Koston <nick@koston.org>
2021-08-02 13:54:50 -0500

    remove

--- src/panels/lovelace/components/hui-image.ts
+++ src/panels/lovelace/components/hui-image.ts
@@ -69,8 +69,6 @@ export class HuiImage extends LitElement {
     this._intersectionObserver = new IntersectionObserver(
       this.handleIntersectionCallback.bind(this),
       {
-        root: document.rootElement,
-        rootMargin: "0px",
         threshold: [0.0],
       }
     );
